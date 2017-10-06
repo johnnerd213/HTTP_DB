073843df9fe0177d745556325c9665bdb39ef795
johnnerd213 <31053915+johnnerd213@users.noreply.github.com>
2017-10-06 11:00:05 +0800

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  baseURL = 'https://first-project-21048.firebaseio.com';
+  baseURL = 'https://first-project-db151.firebaseio.com/';
   rootNode = 'people';
   dataCollection: IPerson[];
 
